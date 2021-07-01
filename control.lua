91d8b995e188b473416b11e68147539347c5afb2
Roger van Bommel <fjvanbommel@gmail.com>
2021-07-01 21:30:09 +0200

    update to 1.0.1

--- control.lua
+++ control.lua
@@ -1,12 +1,14 @@
 local handleBoiler = require("scripts.limited_boilers")
 local handleSolar = require("scripts.limited_solar")
 local function handleBuildByPlayer(e)
-	handleBoiler.handleBuildByPlayer(e)
-	handleSolar.handleBuildByRobot(e)
+	if handleBoiler.handleBuildByPlayer(e) == 0 then
+		handleSolar.handleBuildByPlayer(e)
+	end
 end
 local function handleBuildByRobot(e)
-	handleBoiler.handleBuildByRobot(e)
-	handleSolar.handleBuildByRobot(e)
+	if handleBoiler.handleBuildByRobot(e) == 0 then
+		handleSolar.handleBuildByRobot(e)
+	end
 end
 local function handleDestroy(e)
 	handleBoiler.handleDestroy(e)
@@ -16,7 +18,18 @@ local function setter()
 	handleBoiler.setter()
 	handleSolar.setter()
 end
+local function reset(e)
+	game.print(e.command)
+	if e.command == 'reset' then
+		game.print('resetting counts')
+		global['boiler_count'] = 0
+		global['boiler_ids'] = {}
+		global['solar_count'] = 0
+		global['solar_ids'] = {}
+	end
+end
 script.on_event(defines.events.on_robot_built_entity, handleBuildByRobot)
 script.on_event(defines.events.on_built_entity, handleBuildByPlayer)
 script.on_event(defines.events.on_entity_destroyed,handleDestroy)
+script.on_event(defines.events.on_console_command,reset)
 script.on_init(setter)
--- scripts/limited_boilers.lua
+++ scripts/limited_boilers.lua
@@ -5,21 +5,22 @@ end
 local function handleBuildByPlayer (e)
   if e.created_entity.name == "boiler" then
     global['boiler_count'] = global['boiler_count'] + 1
-	if global['boiler_count'] > 50 then
+	if global['boiler_count'] > 5 then
 		game.print("too many of a limited item have been built, removeing last built and giving to builder")
 		e.created_entity.last_user.insert({name = e.created_entity.name, count=1})
 		e.created_entity.mine({ignore_minable = true})
 		global['boiler_count'] = global['boiler_count'] - 1
-		table.remove(global['boiler_ids'],#global['boiler_ids'])
+		return 1
 	else
 		global['boiler_ids'][global['boiler_count']] = script.register_on_entity_destroyed(e.created_entity)
 	end
   end
+  return 0
 end
 local function handleBuildByRobot (e)
   if e.created_entity.name == "boiler" then
     global['boiler_count'] = global['boiler_count'] + 1
-	if global['boiler_count'] > 50 then
+	if global['boiler_count'] > 5 then
 		local posx = (e.created_entity.bounding_box['left_top']['x']+e.created_entity.bounding_box['right_bottom']['x'])/2
 		local posy = (e.created_entity.bounding_box['left_top']['y']+e.created_entity.bounding_box['right_bottom']['y'])/2
 		local itemstack = {name = e.created_entity.name, count=1}
@@ -28,23 +29,25 @@ local function handleBuildByRobot (e)
 		e.created_entity.mine({ignore_minable = true})
 		surface.spill_item_stack({posx,posy},itemstack)
 		global['boiler_count'] = global['boiler_count'] - 1
-		table.remove(global['boiler_ids'],#global['boiler_ids'])
+		return 1
 	else
 		global['boiler_ids'][global['boiler_count']] = script.register_on_entity_destroyed(e.created_entity)
 	end
   end
+  return 0
 end
 local function handleDestroy (e)
-	for key,value in pairs(global['boiler_ids']) do
+	for key,value in ipairs(global['boiler_ids']) do
 		if e.registration_number == value then
 			global['boiler_count'] = global['boiler_count'] - 1
 		end
 	end	
 end
 local exports = {
-	[handleBuildByRobot] = handleBuildByRobot,
-	[handleBuildByPlayer] = handleBuildByPlayer,
-	[handleDestroy] = handleDestroy,
-	[setter] = setter
+	["handleBuildByRobot"] = handleBuildByRobot,
+	["handleBuildByPlayer"] = handleBuildByPlayer,
+	["handleDestroy"] = handleDestroy,
+	["setter"] = setter
 }
+
 return exports
